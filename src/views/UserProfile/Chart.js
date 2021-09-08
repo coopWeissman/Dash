a3d9f0b539bae0a13380b797108aa1c6578a6e6d
coopWeissman <84481132+coopWeissman@users.noreply.github.com>
2021-09-07 17:57:30 -0700

    new stuff

--- src/views/UserProfile/Chart.js
+++ src/views/UserProfile/Chart.js
@@ -1,47 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTheme } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
 import { BarChart, Legend, Bar, XAxis, YAxis, CartesianGrid } from 'recharts';
 
 
-function Chart() {
+function Chart(props) {
+    const interest = props.interest;
+    // const [years, setYears] = useState(50);
+    var age = props.age;
+    var retirement= props.retirement;
+    var years = retirement-age;
+    var savings = props.savings;
+    var amount = props.amount;
+    var final = props.contibutions;
+    const time = 0 ; 
+    const [id, setId] = useState(3);
     const data = [
-        {
-          "name": "Page A",
-          "uv": 4000,
-          "pv": 2400
-        },
-        {
-          "name": "Page B",
-          "uv": 3000,
-          "pv": 1398
-        },
-        {
-          "name": "Page C",
-          "uv": 2000,
-          "pv": 9800
-        },
-        {
-          "name": "Page D",
-          "uv": 2780,
-          "pv": 3908
-        },
-        {
-          "name": "Page E",
-          "uv": 1890,
-          "pv": 4800
-        },
-        {
-          "name": "Page F",
-          "uv": 2390,
-          "pv": 3800
-        },
-        {
-          "name": "Page G",
-          "uv": 3490,
-          "pv": 4300
-        }
+      
       ]
+
+      for (var i = 0; i < years; i=i+1){
+        age = age+1
+        savings= savings* 1.1
+      data.push(
+        {
+          "name": age,
+          "id": id+i,
+          "uv": 1.1,
+          "pv": amount,
+        }
+      )
+      console.log(data)
+    }
+    
     return (
+      
         <BarChart width={730} height={250} data={data}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
@@ -49,8 +42,10 @@ function Chart() {
         {/* <Tooltip /> */}
         <Legend />
         <Bar dataKey="pv" fill="#8884d8" />
-        <Bar dataKey="uv" fill="#82ca9d" />
+        {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
         </BarChart>
+      
+    
     )
 }
 
---
+++ src/views/UserProfile/array.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function array() {
+    state = {
+        id = 1,
+        person= "jonny"
+    }
+    function newState(){
+        this.setState({person= })
+    }
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default array
